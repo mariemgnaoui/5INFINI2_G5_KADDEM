c9ebb966563b2b433ffcdda5dd3906b85c92ad30
azzouz99 <abderrahmen.benazzouz@esprit.tn>
2024-02-12 16:37:20 +0100

    l5

--- src/test/java/tn/esprit/spring/kaddem/service/UniversiteServicelmpTest.java
+++ src/test/java/tn/esprit/spring/kaddem/service/UniversiteServicelmpTest.java
@@ -23,7 +23,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
-@SpringBootTest
+
 @ExtendWith(MockitoExtension.class)
 public class UniversiteServicelmpTest {
 
